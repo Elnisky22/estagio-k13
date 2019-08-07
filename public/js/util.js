0b59c147eab945b6b5564a26fe7092b9140fe065
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-08-07 18:19:15 -0300

    Slides fixed

--- public/js/util.js
+++ public/js/util.js
@@ -1,30 +1,39 @@
-$(document).ready(function(){
+jQuery(function(){
     $('.produtos').slick({
-        slidsToShow     : 4,
-        slidesToScroll  : 4,
-        infinite        : false,
-        responsive      : [
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: 
+        [
             {
-                breakpoint: 1200,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow    : 3,
-                    slidesToScroll  : 3
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
                 }
             },
             {
-                breakpoint: 997,
+                breakpoint: 600,
                 settings: {
-                    slidesToShow    : 2,
-                    slidesToScroll  : 2
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 500,
+                breakpoint: 480,
                 settings: {
-                    slidesToShow    : 1,
-                    slidesToScroll  : 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
-            ]
+        ]
     });
+});
+
+jQuery(function() {
+    $('.produtos').show();
 });
