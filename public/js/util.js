bb07f831d88f188a05ffc143de6c5bc587a7b8d0
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-08-07 11:54:39 -0300

    Cards updated

--- public/js/util.js
+++ public/js/util.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
-    $('.ofertas').slick({
+    $('.produtos').slick({
         slidsToShow     : 4,
         slidesToScroll  : 4,
         infinite        : false,
         responsive      : [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow    : 3,
-                slidesToScroll  : 3
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow    : 3,
+                    slidesToScroll  : 3
+                }
+            },
+            {
+                breakpoint: 997,
+                settings: {
+                    slidesToShow    : 2,
+                    slidesToScroll  : 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow    : 1,
+                    slidesToScroll  : 1
+                }
             }
-        },
-        {
-            breakpoint: 997,
-            settings: {
-                slidesToShow    : 2,
-                slidesToScroll  : 2
-            }
-        },
-        {
-            breakpoint: 500,
-            settings: {
-                slidesToShow    : 1,
-                slidesToScroll  : 1
-            }
-        }
-        ]
+            ]
     });
 });
