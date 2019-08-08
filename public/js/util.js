7e190f95ba6d3ea67fed6d4eb8c29afc18849502
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-08-08 08:33:15 -0300

    Go to top button

--- public/js/util.js
+++ public/js/util.js
@@ -54,6 +54,22 @@ jQuery(function(){
     });
 });
 
+window.onscroll = function() {scrollFunction()};
+            
+            function scrollFunction() {
+              if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("scrollBtn").style.display = "block";
+              } else {
+                document.getElementById("scrollBtn").style.display = "none";
+              }
+            }
+            
+            // When the user clicks on the button, scroll to the top of the document
+            function topFunction() {
+              document.body.scrollTop = 0;
+              document.documentElement.scrollTop = 0;
+            }
+
 jQuery(function() {
     $('.produtos').show();
     $('.destaques').show();
